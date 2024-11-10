01d4032260695c70d0e7f60cf8292f081ab0d273
jaialo04-ucm-es <jaialo04@ucm.es>
2024-11-10 21:58:02 +0100

    Creación de la BD con todas las relaciones (Susceptible a cambios de tipos)

---
+++ aw_24.sql
@@ -0,0 +1,197 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 10, 2024 at 09:56 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `aw_24`
+--
+CREATE DATABASE IF NOT EXISTS `aw_24` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `aw_24`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `configuraciones_accesibilidad`
+--
+
+CREATE TABLE `configuraciones_accesibilidad` (
+  `id` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `paleta` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`paleta`)),
+  `tamanyo_texto` int(11) NOT NULL,
+  `configuracion_navegacion` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`configuracion_navegacion`))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `eventos`
+--
+
+CREATE TABLE `eventos` (
+  `id` int(11) NOT NULL,
+  `titulo` varchar(100) NOT NULL,
+  `descripcion` varchar(1000) NOT NULL,
+  `fecha` date NOT NULL,
+  `hora` time NOT NULL,
+  `ubicacion` varchar(200) NOT NULL,
+  `capacidad_maxima` int(11) NOT NULL,
+  `id_organizador` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `facultades`
+--
+
+CREATE TABLE `facultades` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `inscripciones`
+--
+
+CREATE TABLE `inscripciones` (
+  `id_organizador` int(11) NOT NULL,
+  `id_evento` int(11) NOT NULL,
+  `esta_lista_espera` tinyint(1) DEFAULT NULL,
+  `fecha_inscripcion` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(20) NOT NULL,
+  `correo` varchar(50) NOT NULL,
+  `telefono` int(11) NOT NULL,
+  `es_organizador` tinyint(1) NOT NULL,
+  `id_facultad` int(11) NOT NULL,
+  `id_accesibilidad` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `configuraciones_accesibilidad`
+--
+ALTER TABLE `configuraciones_accesibilidad`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_idUsuario` (`id_usuario`);
+
+--
+-- Indexes for table `eventos`
+--
+ALTER TABLE `eventos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_idOrganizadorEvento` (`id_organizador`);
+
+--
+-- Indexes for table `facultades`
+--
+ALTER TABLE `facultades`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `inscripciones`
+--
+ALTER TABLE `inscripciones`
+  ADD PRIMARY KEY (`id_organizador`,`id_evento`),
+  ADD KEY `FK_idEvento` (`id_evento`);
+
+--
+-- Indexes for table `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_idFacultad` (`id_facultad`),
+  ADD KEY `FK_idAccesibilidad` (`id_accesibilidad`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `configuraciones_accesibilidad`
+--
+ALTER TABLE `configuraciones_accesibilidad`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `eventos`
+--
+ALTER TABLE `eventos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `facultades`
+--
+ALTER TABLE `facultades`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `configuraciones_accesibilidad`
+--
+ALTER TABLE `configuraciones_accesibilidad`
+  ADD CONSTRAINT `FK_idUsuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`);
+
+--
+-- Constraints for table `eventos`
+--
+ALTER TABLE `eventos`
+  ADD CONSTRAINT `FK_idOrganizadorEvento` FOREIGN KEY (`id_organizador`) REFERENCES `usuarios` (`id`);
+
+--
+-- Constraints for table `inscripciones`
+--
+ALTER TABLE `inscripciones`
+  ADD CONSTRAINT `FK_idEvento` FOREIGN KEY (`id_evento`) REFERENCES `eventos` (`id`),
+  ADD CONSTRAINT `FK_idOrganizador` FOREIGN KEY (`id_organizador`) REFERENCES `usuarios` (`id`);
+
+--
+-- Constraints for table `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD CONSTRAINT `FK_idAccesibilidad` FOREIGN KEY (`id_accesibilidad`) REFERENCES `configuraciones_accesibilidad` (`id`),
+  ADD CONSTRAINT `FK_idFacultad` FOREIGN KEY (`id_facultad`) REFERENCES `facultades` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
